24a53e35cc9bff611b89e5ecd3c85eecdd60d086
Bishal Giri <bishal.giri@neesum.com>
2020-08-29 02:12:54 +0545

    first version of no scanner java code

---
+++ src/com/Bishal/Pratice/Dscanner.java
@@ -0,0 +1,22 @@
+package com.Bishal.Pratice;
+
+public class Dscanner {
+
+
+   /* public String nextChar(String a) throws Exception{
+    char b;
+    while((b=(char)System.in.read())!=(char)10){
+        a += b;
+    }
+    return a;
+    }*/
+
+    /*public String nextChar(String Input) throws Exception{
+        char asci;
+        while((asci = (char)System.in.read())!=(char)10){
+            Input += asci;
+        }
+        return Input;
+    }*/
+}
+
---
+++ src/com/Bishal/Pratice/MySweetProgram.java
@@ -0,0 +1,21 @@
+package com.Bishal.Pratice;
+
+
+public class MySweetProgram {
+    public static void main(String[] args) {
+
+        /*Dscanner imp = new Dscanner();
+        System.out.print("Enter a string to show it in next line:");
+        String someString = imp.nextChar("");
+        System.out.println("output :"+someString);
+        */
+        Dscanner imp = new Dscanner();
+
+           /*String a="";
+           char b;
+           while((b=(char)System.in.read())!=(char)10 ){
+               a += b;
+           }
+            System.out.println(a);*/
+    }
+}
