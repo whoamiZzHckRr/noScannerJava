211b1dc737b5a427b783dc5cb232ec2726462206
Bishal Giri <bishal.giri@neesum.com>
2020-08-29 02:48:19 +0545

    commiting same file third time only after removing comments

--- src/com/Bishal/Pratice/Dscanner.java
+++ src/com/Bishal/Pratice/Dscanner.java
@@ -9,21 +9,5 @@ public class Dscanner {
         }
         return userInput;
     }
-
-   /* public String nextChar(String a) throws Exception{
-    char b;
-    while((b=(char)System.in.read())!=(char)10){
-        a += b;
-    }
-    return a;
-    }*/
-
-    /*public String nextChar(String Input) throws Exception{
-        char asci;
-        while((asci = (char)System.in.read())!=(char)10){
-            Input += asci;
-        }
-        return Input;
-    }*/
 }
 
--- src/com/Bishal/Pratice/MySweetProgram.java
+++ src/com/Bishal/Pratice/MySweetProgram.java
@@ -4,22 +4,10 @@ package com.Bishal.Pratice;
 public class MySweetProgram {
     public static void main(String[] args) throws Exception {
 
-        /*Dscanner imp = new Dscanner();
-        System.out.print("Enter a string to show it in next line:");
-        String someString = imp.nextChar("");
-        System.out.println("output :"+someString);
-        */
         Dscanner imp = new Dscanner();
         System.out.print("Enter a string to show it in next line: ");
         String userInput = imp.nextChar("");
         System.out.println("You have Entered : "+ userInput);
-
-           /*String a="";
-           char b;
-           while((b=(char)System.in.read())!=(char)10 ){
-               a += b;
-           }
-            System.out.println(a);*/
     }
 
 }
